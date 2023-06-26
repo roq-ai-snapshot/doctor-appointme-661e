06d7c458ff17c091d3fef95b762dd13e37669a33
roq-ai-snapshot <fabian.wesner@roq.tech>
2023-06-26 07:21:56 +0000

    Initial commit

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,52 @@
+-- CreateTable
+CREATE TABLE "academy" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "description" VARCHAR(255),
+    "image" VARCHAR(255),
+    "name" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_id" UUID NOT NULL,
+    "tenant_id" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "academy_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "appointment" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "date_time" TIMESTAMP(6) NOT NULL,
+    "coach_id" UUID NOT NULL,
+    "player_id" UUID NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "appointment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "email" VARCHAR(255) NOT NULL,
+    "firstName" VARCHAR(255),
+    "lastName" VARCHAR(255),
+    "roq_user_id" VARCHAR(255) NOT NULL,
+    "tenant_id" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- AddForeignKey
+ALTER TABLE "academy" ADD CONSTRAINT "academy_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "appointment" ADD CONSTRAINT "appointment_coach_id_fkey" FOREIGN KEY ("coach_id") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "appointment" ADD CONSTRAINT "appointment_player_id_fkey" FOREIGN KEY ("player_id") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
